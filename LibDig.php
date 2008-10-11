b68c46d76b5e87c0b5f82eb55ce6b8edc859bc4e
mpntod <mpntod@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-11 23:21:03 +0000

    Move latest version to 0.31
    git-svn-id: https://plugins.svn.wordpress.org/libdig/tags/0.31@68614 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ LibDig.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: LibDig plugin
+Plugin URI: http://www.flocktogether.org.uk/blog/2008/10/09/libdig-wordpress-plug-in/
+Description: Adds a Lib Dig box to Wordpress posts
+Version: 0.31
+Author: Martin Tod
+Author URI: http://www.martintod.org.uk
+*/
+/*  Copyright 2008  Martin Tod  (email : martin@martintod.org.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+?>
+<?php
+add_filter('the_content','addLibDigBox');
+
+function addLibDigBox($content) {
+	global $wp_query;
+	$post 		= $wp_query->post;
+	$permalink 	= urlencode(get_permalink($post->ID));	
+	$title 		= urlencode($post->post_title);
+	$title	 	= str_replace('+','%20',$title);
+	$f 			= 'left'; # Change 'left' to 'right' if you want the LibDig box to appear on the right side of your posts
+	$digScript = '<script type="text/javascript" src="http://libdig.co.uk/widget.php?f='.urlencode($f).'&amp;u='.$permalink.'&amp;t='.$title.'"></script>';
+	if(!is_page() && !is_feed()):
+		$content = $digScript.$content;
+	endif;
+	return($content);
+}
+
+?>
